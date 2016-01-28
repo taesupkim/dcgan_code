60102d7049b118d64d57e2ce42e341fe969a75de
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-01-28 11:03:07 -0500

    push cifar10 model with random cropping to 16x16

---
+++ cifar10/load.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('..')
+
+import os
+from fuel.datasets.cifar10 import CIFAR10
+from fuel.datasets.hdf5 import H5PYDataset
+from fuel.schemes import ShuffledScheme, SequentialScheme
+from fuel.streams import DataStream
+from fuel.transformers.image import RandomFixedSizeCrop
+
+from lib.config import data_dir
+
+def cifar10(ntrain=None, nval=None, ntest=None, batch_size=128):
+    tr_data = CIFAR10(which_sets=('train',), sources=('features',))
+    te_data = CIFAR10(which_sets=('test',), sources=('features',))
+
+    if ntrain is None:
+        ntrain = tr_data.num_examples
+    if ntest is None:
+        ntest = te_data.num_examples
+    if nval is None:
+        nval = te_data.num_examples
+
+    tr_scheme = ShuffledScheme(examples=ntrain, batch_size=batch_size)
+    tr_stream = DataStream(tr_data, iteration_scheme=tr_scheme)
+    tr_stream = RandomFixedSizeCrop(tr_stream, (16, 16))
+
+    te_scheme = SequentialScheme(examples=ntest, batch_size=batch_size)
+    te_stream = DataStream(te_data, iteration_scheme=te_scheme)
+    te_stream = RandomFixedSizeCrop(te_stream, (16, 16))
+
+    val_scheme = SequentialScheme(examples=nval, batch_size=batch_size)
+    val_stream = DataStream(tr_data, iteration_scheme=val_scheme)
+    val_stream = RandomFixedSizeCrop(val_stream, (16, 16))
+
+    return tr_data, te_data, tr_stream, val_stream, te_stream
