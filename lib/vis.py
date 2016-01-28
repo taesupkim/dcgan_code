b0f0fb95003194ec79999242b1849605178b5873
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-01-28 14:21:36 -0500

    push cifar10 model with random cropping to 16x16

--- lib/vis.py
+++ lib/vis.py
@@ -17,7 +17,7 @@ def color_grid_vis(X, (nh, nw), save_path=None):
     print h, w
     img = np.zeros((h*nh, w*nw, 3))
     for n, x in enumerate(X):
-        print x.shape
+        print n ,x.shape
         j = n/nw
         i = n%nw
         img[j*h:j*h+h, i*w:i*w+w, :] = x
