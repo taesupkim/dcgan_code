99174d969f4a31d62d3e8f75ee2c620a1ae1ab14
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-02-13 18:30:58 -0500

    push cifar10 model with random cropping to 16x16

--- cifar10/energy_rbm_cifar10_3.py
+++ cifar10/energy_rbm_cifar10_3.py
@@ -253,8 +253,8 @@ def train_model(learning_rate=1e-2,
                       + '_NOISE{0:.2f}'.format(float(init_noise)) \
                       + '_DECAY{0:.2f}'.format(float(noise_decay)) \
     # set updates
-    energy_updater    = Adagrad(lr=sharedX(learning_rate), regularizer=Regularizer(l2=lambda_eng), clipnorm=0.0)
-    generator_updater = Adagrad(lr=sharedX(learning_rate), regularizer=Regularizer(l2=lambda_gen), clipnorm=0.0)
+    energy_updater    = Adagrad(lr=sharedX(learning_rate), regularizer=Regularizer(l2=lambda_eng), clipnorm=5000.0)
+    generator_updater = Adagrad(lr=sharedX(learning_rate), regularizer=Regularizer(l2=lambda_gen), clipnorm=500.0)
 
     # compile function
     print 'COMPILING'
