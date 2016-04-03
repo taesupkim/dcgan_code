8b7692745e50a47e5fb92f383ff5831cd2ebfca4
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-03 04:19:40 -0400

    new setup

--- cifar10/energy_rbm_cifar10_0.py
+++ cifar10/energy_rbm_cifar10_0.py
@@ -487,7 +487,7 @@ if __name__=="__main__":
 
     hidden_size_list = [512,]
     num_filters_list = [32,]
-    lr_list          = [1e-5]
+    lr_list          = [1e-6]
     dropout_list     = [False, ]
     lambda_eng_list  = [1e-5]
     lambda_gen_list  = [1e-5]
