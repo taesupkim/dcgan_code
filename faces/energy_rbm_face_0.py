d380f4b95e8d156492698b71fb178803d0818c9f
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-04 09:40:46 -0400

    new setup

--- faces/energy_rbm_face_0.py
+++ faces/energy_rbm_face_0.py
@@ -465,7 +465,7 @@ if __name__=="__main__":
 
     hidden_size_list = [512]
     num_filters_list = [16]
-    lr_list          = [1e-3]
+    lr_list          = [1e-4]
     dropout_list     = [False, ]
     lambda_eng_list  = [1e-5]
     lambda_gen_list  = [1e-5]
@@ -487,9 +487,9 @@ if __name__=="__main__":
                                     model_config_dict['noise_decay']         = noise_decay
 
                                     # set updates
-                                    energy_optimizer    = Adam(lr=sharedX(lr),
+                                    energy_optimizer    = RMSprop(lr=sharedX(lr),
                                                                regularizer=Regularizer(l2=lambda_eng))
-                                    generator_optimizer = Adam(lr=sharedX(lr*10),
+                                    generator_optimizer = RMSprop(lr=sharedX(lr),
                                                                regularizer=Regularizer(l2=lambda_gen))
                                     model_test_name = model_name \
                                                       + '_f{}'.format(int(num_filters)) \
