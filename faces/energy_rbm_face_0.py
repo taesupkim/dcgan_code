92d9cc4faefe075eb78f141fba360fa66ee6cf35
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-13 17:26:34 -0400

    new setup

--- faces/energy_rbm_face_0.py
+++ faces/energy_rbm_face_0.py
@@ -492,9 +492,8 @@ if __name__=="__main__":
 
                                     # set updates
                                     energy_optimizer    = RMSprop(lr=sharedX(lr),
-                                                                  rho=0.5,
                                                                   regularizer=Regularizer(l2=lambda_eng))
-                                    generator_optimizer = RMSprop(lr=sharedX(lr*10.0),
+                                    generator_optimizer = RMSprop(lr=sharedX(lr),
                                                                   rho=0.0,
                                                                   regularizer=Regularizer(l2=lambda_gen))
                                     model_test_name = model_name \
