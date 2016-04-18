05fdfc761b1440708653599bd0172046254c962c
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-18 02:25:22 -0400

    new setup

--- faces/energy_rbm_face_0.py
+++ faces/energy_rbm_face_0.py
@@ -500,7 +500,7 @@ if __name__=="__main__":
 
     hidden_size_list = [100]
     num_filters_list = [32]
-    lr_list          = [1e-4]
+    lr_list          = [1e-5]
     dropout_list     = [False, ]
     lambda_eng_list  = [1e-5]
     lambda_gen_list  = [1e-5]
@@ -525,7 +525,7 @@ if __name__=="__main__":
                                     energy_optimizer    = RMSprop(lr=sharedX(lr),
                                                                   rho=0.5,
                                                                   regularizer=Regularizer(l2=lambda_eng))
-                                    generator_optimizer = RMSprop(lr=sharedX(lr),
+                                    generator_optimizer = RMSprop(lr=sharedX(lr*100),
                                                                   rho=0.5,
                                                                   regularizer=Regularizer(l2=lambda_gen))
                                     model_test_name = model_name \
