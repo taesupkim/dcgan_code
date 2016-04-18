b643c0084e85c0ad791be31a23d885417839ed18
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-18 02:19:46 -0400

    new setup

--- faces/energy_rbm_face_0.py
+++ faces/energy_rbm_face_0.py
@@ -500,7 +500,7 @@ if __name__=="__main__":
 
     hidden_size_list = [100]
     num_filters_list = [32]
-    lr_list          = [1e-4]
+    lr_list          = [1e-3]
     dropout_list     = [False, ]
     lambda_eng_list  = [1e-5]
     lambda_gen_list  = [1e-5]
