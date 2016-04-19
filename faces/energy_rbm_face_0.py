8cedb5db6d242ed50f4c99f21edb65f969262be2
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-19 12:50:56 -0400

    new setup

--- faces/energy_rbm_face_0.py
+++ faces/energy_rbm_face_0.py
@@ -224,7 +224,7 @@ def set_energy_update_function(feature_function,
                          dtype=theano.config.floatX)
 
     # annealing scale
-    annealing_scale = 1.0/(1.0+99.0*(0.9**annealing))
+    annealing_scale = 1.0#/(1.0+99.0*(0.9**annealing))
 
     # get sample data
     sample_data = generator_function(hidden_data, is_train=True)
@@ -281,7 +281,7 @@ def set_generator_update_function(feature_function,
                          dtype=theano.config.floatX)
 
     # annealing scale
-    annealing_scale = 1.0/(1.0+99.0*(0.9**annealing))
+    annealing_scale = 1.0#/(1.0+99.0*(0.9**annealing))
 
     # get sample data
     sample_data = generator_function(hidden_data, is_train=True)
