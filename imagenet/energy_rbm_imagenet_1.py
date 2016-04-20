b544198ded48571cab25e4a4041bb9e722999ea5
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-20 12:59:35 -0400

    new setup

--- imagenet/energy_rbm_imagenet_1.py
+++ imagenet/energy_rbm_imagenet_1.py
@@ -21,7 +21,7 @@ def inverse_transform(X):
     X = (X+1.)/2.
     return X
 
-model_name  = 'ENERGY_RBM_IMAGENET_NORMALIZE'
+model_name  = 'ENERGY_RBM_IMAGENET_BATCH_NORM_WITH_BIAS'
 samples_dir = 'samples/%s'%model_name
 if not os.path.exists(samples_dir):
     os.makedirs(samples_dir)
@@ -226,7 +226,7 @@ def set_energy_model(num_hiddens,
         e = softplus(T.dot(feature_data, linear_w0)+linear_b0)
         e = T.sum(-e, axis=1)
         # energy feature prior
-        # e += 0.5*T.sum(T.sqr(feature_std_inv)*T.sqr(feature_data-feature_mean), axis=1)
+        e += 0.5*T.sum(T.sqr(feature_data), axis=1)
         return e
 
     return [feature_function, energy_function, energy_params]
