f381489ccd0de919a48820a53a32c68a431432a0
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-21 07:22:57 -0400

    new setup

--- imagenet/energy_rbm_imagenet_1.py
+++ imagenet/energy_rbm_imagenet_1.py
@@ -464,36 +464,36 @@ def train_model(data_stream,
     [feature_function, energy_function, energy_params] = set_energy_model(model_config_dict['hidden_size'],
                                                                           model_config_dict['min_num_eng_filters'])
 
-    print 'COMPILING UPDATER FUNCTION'
-    t=time()
-    updater_function = set_updater_function(feature_function=feature_function,
-                                            energy_function=energy_function,
-                                            generator_function=generator_function,
-                                            energy_params=energy_params,
-                                            generator_params=generator_params,
-                                            generator_entropy_params=generator_entropy_params,
-                                            energy_optimizer=energy_optimizer,
-                                            generator_optimizer=generator_optimizer)
-    print '%.2f SEC '%(time()-t)
+    # print 'COMPILING UPDATER FUNCTION'
+    # t=time()
+    # updater_function = set_updater_function(feature_function=feature_function,
+    #                                         energy_function=energy_function,
+    #                                         generator_function=generator_function,
+    #                                         energy_params=energy_params,
+    #                                         generator_params=generator_params,
+    #                                         generator_entropy_params=generator_entropy_params,
+    #                                         energy_optimizer=energy_optimizer,
+    #                                         generator_optimizer=generator_optimizer)
+    # print '%.2f SEC '%(time()-t)
 
     # compile functions
-    # print 'COMPILING ENERGY UPDATER'
-    # t=time()
-    # energy_updater = set_energy_update_function(feature_function=feature_function,
-    #                                             energy_function=energy_function,
-    #                                             generator_function=generator_function,
-    #                                             energy_params=energy_params,
-    #                                             energy_optimizer=energy_optimizer)
-    # print '%.2f SEC '%(time()-t)
-    #
-    # print 'COMPILING GENERATOR UPDATER'
-    # t=time()
-    # generator_updater = set_generator_update_function(feature_function=feature_function,
-    #                                                   energy_function=energy_function,
-    #                                                   generator_function=generator_function,
-    #                                                   generator_params=generator_params,
-    #                                                   generator_optimizer=generator_optimizer)
-    # print '%.2f SEC '%(time()-t)
+    print 'COMPILING ENERGY UPDATER'
+    t=time()
+    energy_updater = set_energy_update_function(feature_function=feature_function,
+                                                energy_function=energy_function,
+                                                generator_function=generator_function,
+                                                energy_params=energy_params,
+                                                energy_optimizer=energy_optimizer)
+    print '%.2f SEC '%(time()-t)
+
+    print 'COMPILING GENERATOR UPDATER'
+    t=time()
+    generator_updater = set_generator_update_function(feature_function=feature_function,
+                                                      energy_function=energy_function,
+                                                      generator_function=generator_function,
+                                                      generator_params=generator_params,
+                                                      generator_optimizer=generator_optimizer)
+    print '%.2f SEC '%(time()-t)
 
     # print 'COMPILING EVALUATION FUNCTION'
     # t=time()
@@ -533,7 +533,8 @@ def train_model(data_stream,
             updater_inputs = [input_data,
                               hidden_data,
                               e]
-            updater_outputs = updater_function(*updater_inputs)
+            updater_outputs = generator_updater(*updater_inputs)
+            updater_outputs = energy_updater(*updater_inputs)
 
             # get output values
             input_energy  = updater_outputs[0].mean()
@@ -582,7 +583,7 @@ if __name__=="__main__":
     #################
     _ , data_stream = imagenet(batch_size=model_config_dict['batch_size'])
 
-    hidden_size_list = [512]
+    hidden_size_list = [100]
     num_filters_list = [128]
     lr_list          = [1e-5]
     dropout_list     = [False,]
@@ -606,12 +607,10 @@ if __name__=="__main__":
                                     model_config_dict['noise_decay']         = noise_decay
 
                                     # set updates
-                                    energy_optimizer    = Adam(lr=sharedX(lr),
-                                                               b1=0.5,
-                                                               regularizer=Regularizer(l2=lambda_eng))
-                                    generator_optimizer = Adam(lr=sharedX(lr),
-                                                               b1=0.5,
-                                                               regularizer=Regularizer(l2=lambda_gen))
+                                    energy_optimizer    = RMSprop(lr=sharedX(lr),
+                                                                  regularizer=Regularizer(l2=lambda_eng))
+                                    generator_optimizer = RMSprop(lr=sharedX(lr*2.0),
+                                                                  regularizer=Regularizer(l2=lambda_gen))
                                     model_test_name = model_name \
                                                       + '_f{}'.format(int(num_filters)) \
                                                       + '_h{}'.format(int(hidden_size)) \
