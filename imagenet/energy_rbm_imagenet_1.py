83321a61eaf4a870c3cae4c272749f31a8f3f1f8
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-22 01:34:41 -0400

    new setup

--- imagenet/energy_rbm_imagenet_1.py
+++ imagenet/energy_rbm_imagenet_1.py
@@ -500,7 +500,7 @@ if __name__=="__main__":
 
     hidden_size_list = [1024]
     num_filters_list = [128]
-    lr_list          = [1e-4]
+    lr_list          = [1e-3]
     dropout_list     = [False,]
     lambda_eng_list  = [1e-5]
     lambda_gen_list  = [1e-5]
@@ -518,7 +518,7 @@ if __name__=="__main__":
                             # set updates
                             energy_optimizer    = Adagrad(lr=sharedX(lr),
                                                           regularizer=Regularizer(l2=lambda_eng))
-                            generator_optimizer = Adagrad(lr=sharedX(lr*2),
+                            generator_optimizer = Adagrad(lr=sharedX(lr),
                                                           regularizer=Regularizer(l2=lambda_gen))
                             model_test_name = model_name \
                                               + '_f{}'.format(int(num_filters)) \
