d174da2b46d9e4f0d35a315c7ad0eeaeb7af182e
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-22 06:40:56 -0400

    new setup

--- faces/energy_rbm_face_2.py
+++ faces/energy_rbm_face_2.py
@@ -495,7 +495,7 @@ if __name__=="__main__":
                             # set updates
                             energy_optimizer    = Adagrad(lr=sharedX(lr),
                                                           regularizer=Regularizer(l2=lambda_eng))
-                            generator_optimizer = Adagrad(lr=sharedX(lr*10),
+                            generator_optimizer = Adagrad(lr=sharedX(lr),
                                                           regularizer=Regularizer(l2=lambda_gen))
                             model_test_name = model_name \
                                               + '_f{}'.format(int(num_filters)) \
