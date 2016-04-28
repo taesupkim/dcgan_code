c4b9f6d8db569318910234f16122822c17305d9c
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-28 02:05:10 -0400

    new setup

--- imagenet/energy_rbm_imagenet_2.py
+++ imagenet/energy_rbm_imagenet_2.py
@@ -26,9 +26,11 @@ def inverse_transform(X):
 # SET INITIALIZER #
 ###################
 def get_entropy_cost(entropy_params_list):
+    entropy_const = 0.5*(1.0+np.log(np.pi))
+    entropy_const = entropy_const.astype(floatX)
     entropy_cost = 0.
     for entropy_params in entropy_params_list:
-        entropy_cost += T.sum(T.exp(-2.0*entropy_params))
+        entropy_cost += T.sum(-entropy_const-entropy_params)
     return entropy_cost
 
 def entropy_exp(X, g=None, b=None, u=None, s=None, a=1., e=1e-8):
@@ -583,9 +585,9 @@ if __name__=="__main__":
                                 # set updates
                                 energy_optimizer    = Adagrad(lr=sharedX(lr),
                                                               regularizer=Regularizer(l2=lambda_eng))
-                                generator_optimizer = Adagrad(lr=sharedX(lr),
+                                generator_optimizer = Adagrad(lr=sharedX(lr*10),
                                                               regularizer=Regularizer(l2=lambda_gen))
-                                generator_bn_optimizer = Adagrad(lr=sharedX(lr),
+                                generator_bn_optimizer = Adagrad(lr=sharedX(lr*10),
                                                                  regularizer=Regularizer(l2=0.0))
                                 model_test_name = model_name \
                                                   + '_f{}'.format(int(num_filters)) \
