156493f47791b92fef643834623e5a23e8d33916
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-28 03:25:15 -0400

    new setup

--- imagenet/energy_rbm_imagenet_2.py
+++ imagenet/energy_rbm_imagenet_2.py
@@ -582,7 +582,7 @@ if __name__=="__main__":
     expert_size_list = [1024]
     hidden_size_list = [100]
     num_filters_list = [128]
-    lr_list          = [1e-3]
+    lr_list          = [1e-4]
     dropout_list     = [False,]
     lambda_eng_list  = [1e-10]
     lambda_gen_list  = [1e-10]
