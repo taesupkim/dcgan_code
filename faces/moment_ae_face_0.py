40b7585ea0b636c686b47dc6a5629b3380bcbd94
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-29 03:28:18 -0400

    new setup

--- faces/moment_ae_face_0.py
+++ faces/moment_ae_face_0.py
@@ -533,7 +533,7 @@ if __name__=="__main__":
     _ , data_stream = faces(batch_size=model_config_dict['batch_size'])
 
     hidden_size_list = [100]
-    num_filters_list = [64]
+    num_filters_list = [128]
     lr_list          = [1e-3]
     dropout_list     = [False,]
     lambda_eng_list  = [1e-10]
