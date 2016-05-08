de9b54ad71472abf864a6bde938afef27236c19d
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-05-08 15:37:03 -0400

    new setup

--- cifar10/energy_rbm_cifar10_1.py
+++ cifar10/energy_rbm_cifar10_1.py
@@ -457,7 +457,7 @@ if __name__=="__main__":
     expert_size_list = [2048]
     hidden_size_list = [100]
     num_filters_list = [128]
-    lr_list          = [1e-5]
+    lr_list          = [1e-4]
     lambda_eng_list  = [1e-5]
     lambda_gen_list  = [1e-5]
 
@@ -473,14 +473,20 @@ if __name__=="__main__":
                             model_config_dict['min_num_eng_filters'] = num_filters
 
                             # set updates
-                            energy_optimizer_reg_on  = Adam(lr=sharedX(lr),
-                                                            regularizer=Regularizer(l2=lambda_eng))
-                            generator_optimizer_reg_on  = Adam(lr=sharedX(lr),
-                                                               b1=0.1, b2=0.1,
+                            # energy_optimizer_reg_on  = Adam(lr=sharedX(lr),
+                            #                                 regularizer=Regularizer(l2=lambda_eng))
+                            # generator_optimizer_reg_on  = Adam(lr=sharedX(lr),
+                            #                                    b1=0.1, b2=0.1,
+                            #                                    regularizer=Regularizer(l2=lambda_eng))
+                            # generator_optimizer_reg_off = Adam(lr=sharedX(lr),
+                            #                                    b1=0.1, b2=0.1,
+                            #                                    regularizer=Regularizer(l2=0.0))
+                            energy_optimizer_reg_on  = Adagrad(lr=sharedX(lr),
                                                                regularizer=Regularizer(l2=lambda_eng))
-                            generator_optimizer_reg_off = Adam(lr=sharedX(lr),
-                                                               b1=0.1, b2=0.1,
-                                                               regularizer=Regularizer(l2=0.0))
+                            generator_optimizer_reg_on  = Adagrad(lr=sharedX(2*lr),
+                                                                  regularizer=Regularizer(l2=lambda_eng))
+                            generator_optimizer_reg_off = Adagrad(lr=sharedX(2*lr),
+                                                                  regularizer=Regularizer(l2=0.0))
                             model_test_name = model_name \
                                               + '_f{}'.format(int(num_filters)) \
                                               + '_h{}'.format(int(hidden_size)) \
