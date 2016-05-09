04e6e751e1d3e9434d0baa5c826869aad485ddcc
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-05-09 05:48:07 -0400

    new setup

--- faces/energy_rbm_face_2.py
+++ faces/energy_rbm_face_2.py
@@ -37,7 +37,7 @@ def get_entropy_cost(entropy_params_list):
     entropy_cost = T.sum(-entropy_const-entropy_tensor_params)
     return entropy_cost
 
-model_name  = 'ENERGY_RBM_FACE128_ADAGRAD_NORMED'
+model_name  = 'ENERGY_RBM_FACE128_BIAS_ADAGRAD_NORMED'
 samples_dir = 'samples/%s'%model_name
 if not os.path.exists(samples_dir):
     os.makedirs(samples_dir)
@@ -291,6 +291,7 @@ def set_energy_model(num_experts,
     def energy_function(feature_data, is_train=True):
         e = softplus(T.dot(feature_data, linear_w4)+linear_b4)
         e = T.sum(-e, axis=1)
+        e += 0.5*T.sum(T.sqr(feature_data), axis=1, keepdims=True)
         return e
 
     return [feature_function, energy_function, energy_params]
@@ -717,7 +718,7 @@ if __name__=="__main__":
         expert_size_list = [1024]
         hidden_size_list = [100]
         num_filters_list = [128]
-        lr_list          = [1e-5]
+        lr_list          = [1e-3]
         lambda_eng_list  = [1e-5]
 
         for lr in lr_list:
@@ -733,7 +734,7 @@ if __name__=="__main__":
                             # set updates
                             energy_optimizer    = Adagrad(lr=sharedX(lr),
                                                           regularizer=Regularizer(l2=lambda_eng))
-                            generator_optimizer = Adagrad(lr=sharedX(lr*10.0),
+                            generator_optimizer = Adagrad(lr=sharedX(lr*1.0),
                                                           regularizer=Regularizer(l2=0.0))
                             model_test_name = model_name \
                                               + '_f{}'.format(int(num_filters)) \
