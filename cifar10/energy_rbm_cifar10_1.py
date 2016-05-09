4ad95c4066f8a09eb0a14105be49fd0e1fd2e3e8
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-05-09 07:17:59 -0400

    new setup

--- cifar10/energy_rbm_cifar10_1.py
+++ cifar10/energy_rbm_cifar10_1.py
@@ -449,7 +449,7 @@ if __name__=="__main__":
                         #                                    regularizer=Regularizer(l2=0.0))
                         energy_optimizer    = Adagrad(lr=sharedX(lr),
                                                       regularizer=Regularizer(l2=lambda_eng))
-                        generator_optimizer = Adagrad(lr=sharedX(1*lr),
+                        generator_optimizer = Adagrad(lr=sharedX(10.*lr),
                                                       regularizer=Regularizer(l2=0.0))
                         model_test_name = model_name \
                                           + '_f{}'.format(int(num_filters)) \
