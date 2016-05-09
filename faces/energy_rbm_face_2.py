5faf8e59dc3f87af6b7f47b54ba260eeabf49c63
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-05-09 19:42:18 -0400

    new setup

--- faces/energy_rbm_face_2.py
+++ faces/energy_rbm_face_2.py
@@ -15,7 +15,7 @@ from lib.theano_utils import floatX, sharedX
 from load import faces
 from lib.save_utils import save_model, unpickle
 
-model_name  = 'ENERGY_RBM_FACE128_BIAS_ADAGRAD_NORMED_SEP'
+model_name  = 'ENERGY_RBM_FACE128_BIAS_ADAGRAD_NORMED_ONE_SHOT'
 samples_dir = 'samples/%s'%model_name
 if not os.path.exists(samples_dir):
     os.makedirs(samples_dir)
@@ -267,7 +267,7 @@ def set_energy_model(num_experts,
         # layer 2 (conv)
         h2 = relu(dnn_conv(        h1, conv_w2, subsample=(2, 2), border_mode=(2, 2))+conv_b2.dimshuffle('x', 0, 'x', 'x'))
         # layer 3 (conv)
-        h3 = relu(dnn_conv(        h2, conv_w3, subsample=(2, 2), border_mode=(2, 2))+conv_b3.dimshuffle('x', 0, 'x', 'x'))
+        h3 =      dnn_conv(        h2, conv_w3, subsample=(2, 2), border_mode=(2, 2))+conv_b3.dimshuffle('x', 0, 'x', 'x')
         # output feature
         feature = T.flatten(h3, 2)
         return feature
@@ -276,12 +276,12 @@ def set_energy_model(num_experts,
     print 'SET ENERGY FUNCTION FEATURE NORM LAYER'
     norm_w = scale_ones(input_size,
                         'gen_norm_w')
-    norm_b = bias_zeros(input_size,
-                        'gen_norm_b')
+    # norm_b = bias_zeros(input_size,
+    #                     'gen_norm_b')
 
     def energy_normalize_function(input_data, is_train=True):
         input_data = T.flatten(input_data, 2)
-        return batchnorm(input_data, g=norm_w, b=norm_b)
+        return batchnorm(input_data, g=norm_w)#, b=norm_b)
 
     # ENERGY EXPERT LAYER (LINEAR)
     print 'SET ENERGY FUNCTION EXPERT LAYER'
@@ -305,7 +305,7 @@ def set_energy_model(num_experts,
                      conv_w1, conv_b1,
                      conv_w2, conv_b2,
                      conv_w3, conv_b3,
-                     norm_w, norm_b,
+                     norm_w, #norm_b,
                      expert_w, expert_b]
 
     return [energy_feature_function,
@@ -742,11 +742,6 @@ def train_model(data_stream,
 
             update_input  = [input_data, hidden_data, noise_data]
             update_output = model_updater(*update_input)
-            # if batch_count%2==0:
-            #     update_output = generator_updater(*update_input)
-            # else:
-            #     update_output = energy_updater(*update_input)
-
 
             input_energy    = update_output[0].mean()
             sample_energy   = update_output[1].mean()
