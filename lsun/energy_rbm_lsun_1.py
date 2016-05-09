07ae006cca2e7df82bc207ee0c78a762215ea005
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-05-09 16:27:27 -0400

    new setup

--- lsun/energy_rbm_lsun_1.py
+++ lsun/energy_rbm_lsun_1.py
@@ -814,7 +814,7 @@ if __name__=="__main__":
         expert_size_list = [1024]
         hidden_size_list = [100]
         num_filters_list = [128]
-        lr_list          = [1e-3]
+        lr_list          = [1e-4]
         lambda_eng_list  = [1e-5]
 
         for lr in lr_list:
