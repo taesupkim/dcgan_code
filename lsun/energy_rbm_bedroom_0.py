262f1975e0fc0312642fada1763f7c102b36bb5b
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-05-16 21:54:50 -0400

    new setup

--- lsun/energy_rbm_bedroom_0.py
+++ lsun/energy_rbm_bedroom_0.py
@@ -657,7 +657,7 @@ if __name__=="__main__":
                             # set updates
                             energy_optimizer    = Adagrad(lr=sharedX(lr),
                                                           regularizer=Regularizer(l2=lambda_eng))
-                            generator_optimizer = Adagrad(lr=sharedX(2.*lr))
+                            generator_optimizer = Adagrad(lr=sharedX(5.*lr))
                             model_test_name = model_name \
                                               + '_f{}'.format(int(num_filters)) \
                                               + '_h{}'.format(int(hidden_size)) \
