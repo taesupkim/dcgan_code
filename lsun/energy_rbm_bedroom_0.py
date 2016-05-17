b9f45bc9c12cb26d4f95bac973d7e5621d286ae2
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-05-16 20:08:42 -0400

    new setup

--- lsun/energy_rbm_bedroom_0.py
+++ lsun/energy_rbm_bedroom_0.py
@@ -16,7 +16,7 @@ from load import bedroom
 from lib.save_utils import save_model, unpickle
 t_floatX = theano.config.floatX
 
-model_name  = 'ENERGY_RBM_BEDROOM_TANH_NIPS'
+model_name  = 'ENERGY_RBM_BEDROOM_RELU_NIPS'
 samples_dir = 'samples/%s'%model_name
 if not os.path.exists(samples_dir):
     os.makedirs(samples_dir)
@@ -284,7 +284,7 @@ def set_energy_model(num_experts,
         # layer 2 (conv)
         h2 = relu(dnn_conv(        h1, conv_w2, subsample=(2, 2), border_mode=(2, 2))+conv_b2.dimshuffle('x', 0, 'x', 'x'))
         # layer 3 (conv)
-        h3 = tanh(dnn_conv(        h2, conv_w3, subsample=(2, 2), border_mode=(2, 2))+conv_b3.dimshuffle('x', 0, 'x', 'x'))
+        h3 = relu(dnn_conv(        h2, conv_w3, subsample=(2, 2), border_mode=(2, 2))+conv_b3.dimshuffle('x', 0, 'x', 'x'))
         # output feature
         feature = T.flatten(h3, 2)
         return feature
